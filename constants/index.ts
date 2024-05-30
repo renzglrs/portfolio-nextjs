a671f0204d4a9571b57de9e2eb732b39cc780764
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-05-31 07:28:39 +0800

    refactor: fixed content, deleted unecessary files, chore: added project images

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,10 @@
 export const headerDetails = {
     title: "Renz Glorioso",
     intro: "I am a ",
-    codeText: "<Full Stack Web Developer /> ",
+    codeText: "<Frontend Web Developer /> ",
     location: "based in the Philippines. Explore my work and let's connect for exciting opportunities!",
     className: "inline text-slate-700 font-medium",
-    description: "I am a <Full Stack Web Developer /> based in the Philippines. Explore my work and let's connect for exciting opportunities!"
+    description: "I am a <Frontend Web Developer /> based in the Philippines. Explore my work and let's connect for exciting opportunities!"
 }
 
 export const navLinks = [
@@ -13,6 +13,5 @@ export const navLinks = [
     { label: "Blog", route: "/blog"},
     { label: "Contact", route: "/contact"},
 ];
-
 
 
