d9830c342e8a1f2b104b04408362f61a865226ff
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-06-01 11:44:09 +0800

    feat: WIP added badge for projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {
   Card,
   CardContent,
@@ -7,8 +6,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/app/components/ui/card"
+import { Badge } from "@/app/components/ui/badge"
+import { FaGithub, FaLink } from "react-icons/fa";
+
+
 import img from "@/public/projects/footwork.png"
 import Image from 'next/image'
+import Link from "next/link"
 
 
 // const bg = "./public/projects/footwork.png"
@@ -16,41 +20,44 @@ import Image from 'next/image'
 const ProjectsPage = () => {
   return (
     <>
-      <section className="container max-w-[700px] pt-16">
-        <h1 className="text-3xl font-bold">Projects</h1>
-      </section>
-
       <main className="container max-w-[700px] flex flex-col gap-10 pt-16 pb-40">
+          <h1 className="text-3xl font-bold">Projects</h1>
+
           <ul className='animated-list grid auto-rows md:grid-cols-2 gap-4 mb-10'>
-            <li className='row-span-1 md:col-span-1 rounded-lg hover:bg-gray-200'>
+            <li className='row-span-1 md:col-span-1 rounded-lg hover:bg-secondary'>
               <CardHeader className=''>
                 <CardTitle>Project 1</CardTitle>
                 <CardDescription>Card Description</CardDescription>
               </CardHeader>
-              <CardContent>
+              <CardContent className="flex flex-col gap-3">
+                <div className="flex gap-2">
+                  <Badge>Badge</Badge>
+                  <Badge>Badge</Badge>
+                  <Badge>Badge</Badge>
+                </div>
                 <Image src={img} alt='img' className="rounded-sm" />
                 {/* <p>Card Content</p> */}
               </CardContent>
-              <CardFooter>
-                {/* <p>Card Footer</p> */}
+              <CardFooter className="flex gap-3 text-2xl">
+                <Link href={'/'}><FaGithub /></Link>
+                <Link href={'/'}><FaLink /></Link>
               </CardFooter>
             </li>
 
-            <li className={`row-span-1 md:col-span-1 rounded-lg hover:bg-gray-200`}>
+            <li className={`row-span-1 md:col-span-1 rounded-lg hover:bg-secondary`}>
               <CardHeader>
                 <CardTitle>Project 2</CardTitle>
                 <CardDescription>Card Description</CardDescription>
               </CardHeader>
               <CardContent>
                 <Image src={img} alt='img' className="rounded-sm" />
-                {/* <p>Card Content</p> */}
               </CardContent>
               <CardFooter>
                 {/* <p>Card Footer</p> */}
               </CardFooter>
             </li>
             
-            <li className={`row-span-1 md:col-span-1 rounded-lg hover:bg-gray-200`}>
+            <li className={`row-span-1 md:col-span-1 rounded-lg hover:bg-secondary`}>
               <CardHeader>
                 <CardTitle>Project 3</CardTitle>
                 <CardDescription>Card Description</CardDescription>
